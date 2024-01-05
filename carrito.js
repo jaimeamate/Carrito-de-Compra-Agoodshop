06665732bd1c86d2b5d9a0f06045bb86667b7949
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-01-05 14:06:58 +0100

    js anazado y fetch

--- carrito.js
+++ carrito.js
@@ -1,12 +1,25 @@
 class Carrito {
     constructor(productos) {
+      this.productos = productos
     }
 
     actualizarUnidades(sku, unidades) {
+      const productoEncontrado = this.productos.filter(producto => producto.sku === sku)
+      if (productoEncontrado.length > 0) {
+        productoEncontrado[0].quantity = unidades;  
+        console.log('Producto con sku ${sku} actualizado, unidades actuales: $(productoEncontrado[0].quantity)')        
+      } else {
+        console.log('Producto con sku $(sku) no se ha encontrado')
+      }
       // Actualiza el número de unidades que se quieren comprar de un producto
     }
 
     obtenerInformacionProducto(sku) {
+      const productoEncontrado = this.productos.filter(producto => producto.sku === sku)
+      console.log(productoEncontrado.length > 0
+        ? "Producto encontrado: " + productoEncontrado[0]
+        : "Producto no encontrado"
+      );      
       // Devuelve los datos de un producto además de las unidades seleccionadas
       // Por ejemplo
       // {
@@ -16,9 +29,11 @@ class Carrito {
     }
 
     obtenerCarrito() {
-        fetch('http://jsonblob.com/api/jsonBlob/1184215894598737920')
-        .then(response => response.json())
-        .then(products => {console.log(products)});
+      // fetch('https://jsonblob.com/api/jsonBlob/1192490599940218880')
+      fetch('data.json')
+      .then(promise => promise.json().then(function(result) {
+        console.log(result);
+      }));      
       // Devuelve información de los productos añadidos al carrito
       // Además del total calculado de todos los productos
       // Por ejemplo:
@@ -31,6 +46,6 @@ class Carrito {
       //       ..
       //     }
       //    ]}
-      // }
+      // }  
     }
   }
